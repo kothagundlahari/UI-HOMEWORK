d7ddc8df68be45328ec9d787487b3961c5d945e6
hari krisha <hari.k.krishna4@gmail.com>
2016-11-19 15:36:39 -0600

    something happening in isolated

---
+++ HomeWork20/IsolatedScope/controllers.js
@@ -0,0 +1,21 @@
+
+var app=angular.module('app', []);
+app.controller('message', ['$scope', function($scope){
+	$scope.a=10;
+	$scope.b=20;
+	$scope.p=30;
+	$scope.q=40;
+
+	
+}]);
+app.directive('message',function(){
+	return{
+			templateUrl:'some.htm',
+			scope:{
+				x:'@',
+				y:'@',
+
+			},
+	}
+
+});
