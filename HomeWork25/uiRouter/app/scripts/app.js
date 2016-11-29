d5fe337d590d3b09789bf509b7f959879f7d63ee
hari krisha <hari.k.krishna4@gmail.com>
2016-11-28 23:35:15 -0600

    google

---
+++ HomeWork25/uiRouter/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name uiRouterApp
+ * @description
+ * # uiRouterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uiRouterApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
